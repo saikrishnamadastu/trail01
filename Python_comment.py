c0b92e5c69b23af5c3b554784eff6ce99ecaf413
SaiKrishna <saikrishna.madastu@gmail.com>
2020-10-18 23:21:16 -0500

    added python comment file

---
+++ Python_comment.py
@@ -0,0 +1,7 @@
+# is used for comment in Python
+print("# is used for commenting in Python")
+""" Docs string comments are used for multi line commenting
+but the interpretor doesn't ignore the text
+commad to see hidden files in a folder of mac, first makesure you are in the folder terminal
+defaults write com.apple.finder AppleShowAllFiles YES
+"""
